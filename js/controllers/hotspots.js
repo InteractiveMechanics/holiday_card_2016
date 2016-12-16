b4b6e9b6d0777a630fbe24d85233468d5ff1bff5
Christina Deemer <cahdeemer@gmail.com>
2016-12-16 14:13:25 -0500

    commit for jeff

--- js/controllers/hotspots.js
+++ js/controllers/hotspots.js
@@ -3,7 +3,7 @@ Hotspots = (function() {
         bindEvents();
     }
     
-    
+
     var bindEvents = function() {
         $(document).on('click tap', '.chair', nextImg);
         $(document).on('click tap', '.fireplace', nextImg);
@@ -12,6 +12,7 @@ Hotspots = (function() {
         $(document).on('click tap', '.pet', nextImg);
         $(document).on('click tap', '.table', nextImg);
         $(document).on('click tap', '.tree', nextImg);
+        $(document).on('click tap', '#testingBtn', getIndex);
         
     }
 
@@ -20,12 +21,42 @@ Hotspots = (function() {
     		$(this).removeClass('active').addClass('fade').addClass('hidden');
     		if($(this).next().length > 0) {
     			$(this).next().addClass('active').removeClass('hidden').removeClass('fade').addClass('in');
+    			
     		} else {
     			$(this).parent().find('img').first().addClass('active').removeClass('hidden').removeClass('fade').addClass('in');
     		}
     		
     	}
     }
+
+
+    var updateQueryStringParameter = function(uri, key, value){
+	  	var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+	  	var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+	  	if (uri.match(re)) {
+	    	return uri.replace(re, '$1' + key + "=" + value + '$2');
+	  	} else {
+	    	return uri + separator + key + "=" + value;
+	  	}
+	}
+
+
+
+
+    var chairIndex = $('#chair img.active').index();
+    var fireplaceIndex = $('#fireplace img.active').index();
+    var mantelIndex = $('#mantel img.active').index();
+
+    var getIndex = function() {
+    	alert(chairIndex);
+    	alert(fireplaceIndex);
+    	alert(mantelIndex);
+    }
+
+
+
+
+
 
     
 
