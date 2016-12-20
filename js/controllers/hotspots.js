a10de0553035c882fd2aa333a92d2b8d472fda8e
Christina Deemer <cahdeemer@gmail.com>
2016-12-20 17:48:06 -0500

    added icons and build scene stuff

--- js/controllers/hotspots.js
+++ js/controllers/hotspots.js
@@ -1,8 +1,51 @@
 Hotspots = (function() {
     var init = function() {
+        setIndex();
         bindEvents();
     }
     
+
+    var setIndex = function() {
+        var settingsString = getParameterByName('settings');
+        var settingsArray = settingsString.split(',');
+        if (settingsString.length == 0 || settingsString == null) {
+            setDefaultScene();
+        } else {
+            $('#chair img:eq(' + validateSettings(settingsArray[0]) + ')').addClass('active in').removeClass('hidden fade');
+            $('#fireplace img:eq(' + validateSettings(settingsArray[0]) + ')').addClass('active in').removeClass('hidden fade');
+            $('#mantel img:eq(' + validateSettings(settingsArray[0]) + ')').addClass('active in').removeClass('hidden fade');
+            $('#mantelGarland img:eq(' + validateSettings(settingsArray[0]) + ')').addClass('active in').removeClass('hidden fade');
+            $('#pet img:eq(' + validateSettings(settingsArray[0]) + ')').addClass('active in').removeClass('hidden fade');
+            $('#table img:eq(' + validateSettings(settingsArray[0]) + ')').addClass('active in').removeClass('hidden fade');
+            $('#tree img:eq(' + validateSettings(settingsArray[0]) + ')').addClass('active in').removeClass('hidden fade');
+        }
+    }
+
+    var setDefaultScene = function() {
+        $('#chair img:eq(0)').addClass('active in').removeClass('hidden fade');
+        $('#fireplace img:eq(0)').addClass('active in').removeClass('hidden fade');
+        $('#mantel img:eq(0)').addClass('active in').removeClass('hidden fade');
+        $('#mantelGarland img:eq(0)').addClass('active in').removeClass('hidden fade');
+        $('#pet img:eq(0)').addClass('active in').removeClass('hidden fade');
+        $('#table img:eq(0)').addClass('active in').removeClass('hidden fade');
+        $('#tree img:eq(0)').addClass('active in').removeClass('hidden fade');
+    }
+
+
+    var validateSettings = function(setting) {
+        if (setting >= 0 && setting <= 2) {
+            return setting;
+        } else {
+            return 0;
+        }
+    }
+
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
 
     var bindEvents = function() {
         $(document).on('click tap', '.chair', nextImg);
@@ -18,7 +61,7 @@ Hotspots = (function() {
 
     var nextImg = function() {
     	if($(this).hasClass('active')) {
-    		$(this).removeClass('active').addClass('fade').addClass('hidden');
+    		$(this).removeClass('active').addClass('fade').removeClass('in').addClass('hidden');
     		if($(this).next().length > 0) {
     			$(this).next().addClass('active').removeClass('hidden').removeClass('fade').addClass('in');
     			
@@ -30,28 +73,44 @@ Hotspots = (function() {
     }
 
 
-    var updateQueryStringParameter = function(uri, key, value){
-	  	var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-	  	var separator = uri.indexOf('?') !== -1 ? "&" : "?";
-	  	if (uri.match(re)) {
-	    	return uri.replace(re, '$1' + key + "=" + value + '$2');
-	  	} else {
-	    	return uri + separator + key + "=" + value;
-	  	}
-	}
+	var myArray = [];
 
-
-
-
-    var chairIndex = $('#chair img.active').index();
-    var fireplaceIndex = $('#fireplace img.active').index();
-    var mantelIndex = $('#mantel img.active').index();
+    var chairIndex, fireplaceIndex, mantelIndex, mantelGarlandIndex, petIndex, tableIndex, treeIndex;
+   
 
     var getIndex = function() {
-    	alert(chairIndex);
-    	alert(fireplaceIndex);
-    	alert(mantelIndex);
+       
+
+    	chairIndex = $('#chair img.active').index();
+    	fireplaceIndex = $('#fireplace img.active').index();
+    	mantelIndex = $('#mantel img.active').index();
+    	mantelGarlandIndex = $('#mantelGarland img.active').index();
+    	petIndex = $('#pet img.active').index();
+    	tableIndex = $('#table img.active').index();
+    	treeIndex = $('#tree img.active').index();
+
+    	if (myArray === !undefined || myArray.length == !0) {
+    		myArray.splice(0, 7, chairIndex, fireplaceIndex, mantelIndex, mantelGarlandIndex, petIndex, tableIndex, treeIndex);
+    	} else {
+    		myArray.push(chairIndex, fireplaceIndex, mantelIndex, mantelGarlandIndex, petIndex, tableIndex, treeIndex);
+    	}
+
+    	
+        
+      
+
+    	// alert('myArray: ' + myArray);
     }
+
+    var facebooklink = 'https://www.facebook.com/sharer/sharer.php?u=' + window.location.protocol + "//" + window.location.host + window.location.pathname + '?settings=' + myArray;
+
+    var updateURL = function() {
+    	var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?settings=' + myArray;
+        window.history.pushState({path:newurl},'',newurl);
+    }
+
+    
+
 
 
 
--- js/controllers/utilities.js
+++ js/controllers/utilities.js
@@ -5,7 +5,12 @@ Utilities = (function() {
     }
    
     var bindEvents = function() {
+    	$(document).ready(resetURL);
         
+    }
+
+    var resetURL = function() {
+    	location.replace('localhost:4000');
     }
     
     return {
