41505e639f55ddbf30ab9da778773178f8dd2eb1
Christina Deemer <cahdeemer@gmail.com>
2016-12-23 10:48:44 -0500

    working fire

--- js/controllers/fire.js
+++ js/controllers/fire.js
@@ -6,54 +6,6 @@ Fire = (function() {
    
     var bindEvents = function() {
     }
-
-
-    var canvas = document.querySelector("#fire");
-    var context = canvas.getContext("2d");
-    var myImage = new Image();
-    myImage.src = "./assets/fire.png";
-    myImage.addEventListener("load", loadImage, false);
-
-    function loadImage(e) {
-        animate();
-    }
- 
-    var shift = 0;
-    var frameWidth = 120;
-    var frameHeight = 120;
-    var totalFrames = 9;
-    var currentFrame = 0;
-    var lastAdvance = 0;
- 
-    function animate() {
-        context.clearRect(0, 0, 120, 120);
- 
-        //draw each frame + place them in the middle
-        context.drawImage(myImage, shift, 0, frameWidth, frameHeight,
-                        0, 0, frameWidth, frameHeight);
-     
-        shift += frameWidth + 1;
-     
-        /*
-            Start at the beginning once you've reached the
-            end of your sprite!
-        */
-        if (currentFrame == totalFrames) {
-            shift = 0;
-            currentFrame = 0;
-        }
-     
-        currentFrame++;
-     
-    
-        requestAnimationFrame(animate);
-
-
-        
-    }
-
-
-    
     
     return {
         init: init
