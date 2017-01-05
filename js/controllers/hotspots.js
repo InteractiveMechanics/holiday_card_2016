ffea1673f107bd9718db9d967ae43e8bf6abbcc1
Christina Deemer <cahdeemer@gmail.com>
2017-01-05 15:04:53 -0500

    update

--- js/controllers/hotspots.js
+++ js/controllers/hotspots.js
@@ -101,20 +101,20 @@ Hotspots = (function() {
     	}
     }
 
-    var facebooklink = 'https://www.facebook.com/sharer/sharer.php?u=' + window.location.protocol + "//" + window.location.host + window.location.pathname + '?settings=' + myArray;
+    var facebooklink = 'https://www.facebook.com/sharer/sharer.php?u=' + window.location.protocol + "//" + window.location.host + window.location.pathname + '?setting=' + myArray;
 
    
 
     var sendTweet = function() {
         getIndex();
-        var twitterlink = 'https://twitter.com/home?status=Check%20out%20my%20custom%20New%20Year%E2%80%99s%20scene%20%26%20design%20your%20own%202017%20with%20%40InteractiveMech%20%23happynewyear%20%23newyeardoyou%0A' + window.location.protocol + "//" + window.location.host + window.location.pathname + '?settings=' + myArray;
+        var twitterlink = 'https://twitter.com/home?status=Check%20out%20my%20custom%20New%20Year%E2%80%99s%20scene%20%26%20design%20your%20own%202017%20with%20%40InteractiveMech%20%23happynewyear%20%23newyeardoyou%0A' + window.location.protocol + "//" + window.location.host + window.location.pathname + '?setting=' + myArray;
         window.open(twitterlink,'_blank');
         sendToTwitter();
     }
 
     var sendFacebook = function() {
         getIndex();
-        var facebooklink = window.location.protocol + "//" + window.location.host + window.location.pathname + '?settings=' + myArray;;
+        var facebooklink = window.location.protocol + "//" + window.location.host + window.location.pathname + '?setting=' + myArray;
     
          $.getJSON(
             "http://api.bitly.com/v3/shorten?callback=?", 
@@ -143,13 +143,13 @@ Hotspots = (function() {
     }
 
     var updateURL = function() {
-    	var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?settings=' + myArray;
+    	var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?setting=' + myArray;
         window.history.pushState({path:newurl},'',newurl);
     }
 
     function sendToTwitter() {
     getIndex();
-     var twitterlink = window.location.protocol + "//" + window.location.host + window.location.pathname + '?settings=' + myArray;
+     var twitterlink = window.location.protocol + "//" + window.location.host + window.location.pathname + '?setting=' + myArray;
     console.log(twitterlink);
 
         $.getJSON(
